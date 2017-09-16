60f6d4a9471ad5f5dbde8125f65f09097c3f6f9e
vinicius <vinicius@vinicius>
2017-09-16 20:30:58 -0300

    validacao de genero para livro

--- src/main/java/com/vinicius/pos/service/LivroService.java
+++ src/main/java/com/vinicius/pos/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.vinicius.pos.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vinicius.pos.base.ServiceCrudBase;
@@ -8,6 +9,17 @@ import com.vinicius.pos.repository.LivroRepository;
 
 @Service
 public class LivroService extends ServiceCrudBase<Long, Livro, LivroRepository>{
+
+	@Autowired GeneroService generoService;
+	
+	@Override
+	public Livro inserir(Livro entidade) {
+		generoService.pesguisarPorId(entidade.getGenero().getId());
+		
+		
+		
+		return super.inserir(entidade);
+	}
 	
 	
 	

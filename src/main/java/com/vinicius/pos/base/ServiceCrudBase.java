3d5fc87f8dd6727ff0864208a8510390077adebe
vinicius <vinicius@vinicius>
2017-09-16 20:31:46 -0300

    correcao do crud basico

--- src/main/java/com/vinicius/pos/base/ServiceCrudBase.java
+++ src/main/java/com/vinicius/pos/base/ServiceCrudBase.java
@@ -3,6 +3,7 @@ package com.vinicius.pos.base;
 import java.io.Serializable;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public abstract class ServiceCrudBase <ID extends Serializable, E extends EntityBase<ID>, R extends JpaRepository<E, ID>> {
@@ -18,26 +19,25 @@ public abstract class ServiceCrudBase <ID extends Serializable, E extends Entity
 	}
 	
 	public E alterar( E entidade) {
-		E entidadeAAlterar = this.repository.findOne(entidade.getId());
-		if(entidadeAAlterar == null) {return null;}
-		entidadeAAlterar.setId(entidade.getId());
-		this.repository.save(entidadeAAlterar);
-		return entidadeAAlterar;
+		pesguisarPorId(entidade.getId());
+		this.repository.save(entidade);
+		return entidade;
 	}
 	
 	public E pesguisarPorId(ID id) {
-		return this.repository.findOne(id);
+		 E entidade =  this.repository.findOne(id); 
+		 if(entidade == null ) throw new EmptyResultDataAccessException(0);
+		return entidade;
+		
 	}
 	
 	public List<E>listar() {
 		return this.repository.findAll();
 	}
 	
-	public boolean deletar(ID id) {
-		E entidadeAExcluir = this.repository.findOne(id);
-		if(entidadeAExcluir == null) {return false;}
+	public void deletar(ID id) {
+		pesguisarPorId(id);
 		this.repository.delete(id);
-		return true;
 	}	
 	
 	protected R getRepository() {
